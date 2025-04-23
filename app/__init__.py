2d7771152db64c57b4e7f4263eb8e8bfd5974daf
Pavlovschii Radu <radu.pavlovschi@gmail.com>
2025-04-23 11:09:45 +0300

    formated env file

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://radu.pavlovschi:dodik54@localhost:5432/radu_test'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
