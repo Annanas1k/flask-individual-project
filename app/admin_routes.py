c3b7d72fac64bb076e05ee0353ef7dd824920f4f
Pavlovschii Radu <radu.pavlovschi@gmail.com>
2025-04-25 11:34:33 +0300

    add admin login method + hashing

--- app/admin_routes.py
+++ app/admin_routes.py
@@ -1,15 +1,44 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from dotenv import load_dotenv
 from .models.models import *
-from flask import Blueprint, render_template, redirect, url_for, request, jsonify
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify, flash, session
+
+load_dotenv()
 
 admin = Blueprint('admin', __name__, url_prefix='/admin', template_folder='templates')
 
+ADMIN_LOGIN = os.getenv('ADMIN_LOGIN')
+ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
+
+
 @admin.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+
+        if not login or not password:
+            flash("Datele sunt gresite sau incorecte!!", "error")
+            return redirect(url_for('admin.login'))
+
+        if login == ADMIN_LOGIN and check_password_hash(ADMIN_PASSWORD, password):
+            session['admin'] = True
+            return redirect(url_for('admin.dashboard'))
+        else:
+            flash("Datele sunt gresite!!", "error")
+
+
     return render_template("admin/login.html")
 
 @admin.route('/', methods=['GET', 'POST'])
 @admin.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
+    if "admin" not in session:
+        flash("Sesiunea a expirat! Te rugam sa te loghezi din nou", "error")
+        return redirect(url_for('admin.login'))
+
+
     return render_template("admin/dashboard.html")
 
 @admin.route('/students', methods=['GET', 'POST'])
@@ -31,3 +60,14 @@ def absente():
 @admin.route('/logout')
 def logout():
     return redirect(url_for('admin.login'))
+
+
+
+
+@admin.route('/hashpass', methods=['GET', 'POST'])
+def hashpass():
+    parola = "admin"
+    hash_parola = generate_password_hash(parola)
+    return jsonify({"message": "Parolele au fost hashate și actualizate cu succes!",
+                    "hashed_password:": hash_parola}), 200
+
